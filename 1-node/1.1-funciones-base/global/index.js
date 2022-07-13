6bdbffbf63dcba027e37d436806476afd78f279c
JuanN <nahuat.123@gmail.com>
2022-07-13 12:23:43 -0500

    feat: agrega comandos del modulo global

---
+++ 1-node/1.1-funciones-base/global/index.js
@@ -0,0 +1,5 @@
+console.log('Ruta donde se encuentra mi archivo: ' ,__dirname);
+console.log('Ruta + nombre de mi archivo: ', __filename);
+//exports: Para exportar modulos (Archivos)
+//module: Referencia al modulo actual
+//require(): Importa un archivo js como modulo
